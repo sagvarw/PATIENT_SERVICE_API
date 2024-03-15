99ca1ea27a151cfae3f3c3d4e10aba2f54cd4d3b
Wilendy <146790370+sagvarw@users.noreply.github.com>
2024-03-15 19:20:44 -0400

    Delete 05_alter_add_insurance_id.sql

--- 05_alter_add_insurance_id.sql
+++
@@ -1,4 +0,0 @@
--- Active: 1709770292246@@127.0.0.1@5432@postgres@public
--- Change DOB column to use dates
-ALTER TABLE public."patient" ALTER COLUMN "DOB" TYPE DATE
-USING to_date("DOB"::text, 'YYYY-MM-DD');
