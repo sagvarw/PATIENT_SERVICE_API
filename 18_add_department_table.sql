f513eba988a800ad6fbd3a6cfd9adea97123f419
Wilendy <146790370+sagvarw@users.noreply.github.com>
2024-03-15 19:22:22 -0400

    Delete 18_add_department_table.sql

--- 18_add_department_table.sql
+++
@@ -1,9 +0,0 @@
--- Department Table
--- Each department is related to one hospital,
--- but a hospital can have multiple departments
-CREATE TABLE department (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(255) NOT NULL,
-    "hospital_id" INT NOT NULL,
-    FOREIGN KEY (hospital_id) REFERENCES hospital(id)
-);
