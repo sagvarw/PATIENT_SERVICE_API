74533a7687838947b5e30c4eec8282958e5681ec
Wilendy <146790370+sagvarw@users.noreply.github.com>
2024-03-15 19:22:37 -0400

    Delete 20_add_manager_table.sql

--- 20_add_manager_table.sql
+++
@@ -1,6 +0,0 @@
--- Extension of Employee Table for Managers
--- employees but have specific roles and additional information.
-CREATE TABLE manager (
-    "id" SERIAL PRIMARY KEY,
-    FOREIGN KEY (id) REFERENCES employee(id)
-);
